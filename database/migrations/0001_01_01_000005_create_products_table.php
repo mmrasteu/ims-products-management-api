1ebccee339b8dd63dcd06c3ae0624fa771ac8807
mmtasteu <mmrasteu@gmail.com>
2024-05-06 11:14:06 +0200

    Make migrations

---
+++ database/migrations/0001_01_01_000005_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('sku_code')->unique();
+            $table->string('description');
+            $table->decimal('price', 8, 2);
+            $table->decimal('cost_price', 8, 2);
+            $table->boolean('status')->default(true);
+            $table->json('additional_features');
+            $table->timestamps();
+
+            // Relacion con Categories
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+
+            // Relacion con Suppliers
+            $table->unsignedBigInteger('supplier_id');
+            $table->foreign('supplier_id')
+                ->references('id')
+                ->on('suppliers')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
